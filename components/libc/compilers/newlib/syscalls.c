b81fc6bf96c4540ae55e6f777dd3e4a8a25d60c7
Thomas Hornschuh <thomas@bonfirecpu.eu>
2021-12-30 20:22:11 +0100

    Added #define RT_USING_LIBC which seems to be missing in newest version

--- components/libc/compilers/newlib/syscalls.c
+++ components/libc/compilers/newlib/syscalls.c
@@ -92,6 +92,8 @@ void __libc_init_array(void)
 }
 
 /* Reentrant versions of system calls.  */
+
+#ifdef RT_USING_LIBC // TH: Ported from old version 
 #ifndef _REENT_ONLY
 int *__errno(void)
 {
@@ -322,6 +324,8 @@ _ssize_t _write_r(struct _reent *ptr, int fd, const void *buf, size_t nbytes)
     return -1;
 #endif /* DFS_USING_POSIX */
 }
+
+#endif /* RT_USING_LIBC  */  // TH...
 
 /* for exit() and abort() */
 __attribute__ ((noreturn)) void _exit (int status)
