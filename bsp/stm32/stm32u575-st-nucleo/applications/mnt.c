0197d24e0a934063e66cc1d8879f9a22210bbf38
Thomas Hornschuh <thomas@bonfirecpu.eu>
2022-07-17 20:32:40 +0200

    End of day commot stm32u575 progress

---
+++ bsp/stm32/stm32u575-st-nucleo/applications/mnt.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2019, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2019-09-19     Gavin        first version
+ *
+ */
+
+#include <rtthread.h>
+
+#include <dfs_fs.h>
+
+#ifdef RT_USING_DFS_ROMFS
+
+#include "dfs_romfs.h"
+
+const static struct romfs_dirent _dummy[] =
+{
+//    {ROMFS_DIRENT_FILE, "dummy.txt", _dummy_dummy_txt, sizeof(_dummy_dummy_txt)},
+};
+
+// const static unsigned char _dummy_txt[] =
+// {
+//     0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x61, 0x20, 0x66, 0x69, 0x6c, 0x65, 0x21, 0x0d, 0x0a,
+// };
+
+RT_WEAK const struct romfs_dirent _root_dirent[] =
+{
+    {ROMFS_DIRENT_DIR, "sd", (rt_uint8_t *)_dummy, 0},//  sizeof(_dummy) / sizeof(_dummy[0])},
+	{ROMFS_DIRENT_DIR, "ram", (rt_uint8_t *)_dummy,0} //sizeof(_dummy) / sizeof(_dummy[0])},
+    
+};
+
+RT_WEAK const struct romfs_dirent romfs_root =
+{
+    ROMFS_DIRENT_DIR, "/", (rt_uint8_t *)_root_dirent, sizeof(_root_dirent) / sizeof(_root_dirent[0])
+};
+
+#endif
+
+#ifdef RT_USING_DFS_RAMFS
+extern struct dfs_ramfs *dfs_ramfs_create(rt_uint8_t *pool, rt_size_t size);
+#endif
+
+int mnt_init(void)
+{
+	
+
+    rt_err_t romfs_mounted = RT_ERROR;
+#ifdef RT_USING_DFS_ROMFS
+	  romfs_mounted = dfs_mount(RT_NULL,"/","rom",0,&romfs_root);
+#endif
+
+#ifdef RT_USING_DFS_RAMFS
+	rt_uint8_t *pool = RT_NULL;
+	rt_size_t size = 32768;
+	pool = rt_malloc(size);
+	if (pool == RT_NULL)
+		return 0;
+
+	if (dfs_mount(RT_NULL,romfs_mounted==RT_EOK?"/ram":"/", "ram", 0, (const void *)dfs_ramfs_create(pool, size)) == 0)
+		rt_kprintf("\nRAM file system initializated!\n");
+	else
+		rt_kprintf("\nRAM file system initializate failed!\n");
+
+	return 0;
+#endif	
+}
+INIT_ENV_EXPORT(mnt_init);
+
+#ifdef RT_USING_MSH
+//#pragma message "installing mount_sd"
+
+#include <sys/stat.h>
+
+static void mount_sd(int argc, char** argv)
+{
+struct stat stats;
+const char sd[]="/sd";
+const char *mountpoint = ((stat(sd,&stats)==0) && S_ISDIR(stats.st_mode))?sd:"/";
+
+	rt_err_t err=dfs_mount("sd0",mountpoint,"elm",0,NULL);
+	if (err==RT_EOK) {
+		rt_kprintf("sdcard mounted at %s\n",mountpoint);
+	} else {
+		rt_kprintf("sdcard mount failed with error %d\n",err);
+	}        
+}
+
+MSH_CMD_EXPORT(mount_sd, mount_sd: mount sd0   );
+#endif
+
+
--- bsp/stm32/stm32u575-st-nucleo/rtconfig.h
+++ bsp/stm32/stm32u575-st-nucleo/rtconfig.h
@@ -37,6 +37,8 @@
 
 #define RT_USING_MEMPOOL
 #define RT_USING_SMALL_MEM
+#define RT_USING_MEMHEAP
+#define RT_MEMHEAP_FAST_MODE
 #define RT_USING_SMALL_MEM_AS_HEAP
 #define RT_USING_HEAP
 
@@ -82,6 +84,8 @@
 #define DFS_FILESYSTEMS_MAX 4
 #define DFS_FILESYSTEM_TYPES_MAX 4
 #define DFS_FD_MAX 16
+#define RT_USING_DFS_ROMFS
+#define RT_USING_DFS_RAMFS
 
 /* Device Drivers */
 
@@ -91,11 +95,9 @@
 #define RT_SERIAL_RB_BUFSZ 64
 #define RT_USING_CPUTIME
 #define RT_USING_PIN
+#define RT_USING_RTC
+#define RT_USING_SOFT_RTC
 #define RT_USING_SPI
-#define RT_USING_HWCRYPTO
-#define RT_HWCRYPTO_DEFAULT_NAME "hwcryto"
-#define RT_HWCRYPTO_IV_MAX_SIZE 16
-#define RT_HWCRYPTO_KEYBIT_MAX_SIZE 256
 
 /* Using USB */
 
@@ -105,13 +107,9 @@
 #define USB_VENDOR_ID 0x0FFE
 #define USB_PRODUCT_ID 0x0001
 #define RT_USB_DEVICE_COMPOSITE
-#define RT_USB_DEVICE_CDC
 #define RT_USB_DEVICE_NONE
-#define RT_VCOM_TASK_STK_SIZE 512
-#define RT_CDC_RX_BUFSIZE 128
-#define RT_VCOM_SERNO "32021919830108"
-#define RT_VCOM_SER_LEN 14
-#define RT_VCOM_TX_TIMEOUT 1000
+#define RT_USB_DEVICE_MSTORAGE
+#define RT_USB_MSTORAGE_DISK_NAME "flash0"
 
 /* POSIX layer and C standard library */
 
@@ -230,7 +228,9 @@
 #define BSP_USING_UART1
 #define BSP_USING_SPI
 #define BSP_USING_SPI1
-#define BSP_USING_UDID
+#define BSP_SCB_ENABLE_I_CACHE
+#define BSP_USING_USBD
+#define BSP_USBD_TYPE_FS
 
 /* Board extended module Drivers */
 
